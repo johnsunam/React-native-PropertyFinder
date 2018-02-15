4c5b5aeb8d0e89868696f5afb0a841d459f5faaf
john <sunamjohn@gmail.com>
2018-02-15 22:26:58 +0545

    property finder completed

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 'use strict';
-
-
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation'
-class SearchPage extends Component {
-
-  static navigationOptions ={
-    title:'Property Finder'
-  }
-  render() {
-    return <Text style={styles.description}>Search for the house to buy!(Again)</Text>;
-  }
-}
-
-
+import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
 
 
 
 const App = StackNavigator({
-  Home: {screen: SearchPage}
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults }
 })
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  },
-});
 
 export default App;
